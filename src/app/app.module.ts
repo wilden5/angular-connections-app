3705524af391b9e8d660b63432228dfaf49de317
Denis Ivanchenko <sicknature24@yandex.com>
2023-10-17 12:08:11 +0300

    feat: add project components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { FilterComponent } from './header/filter/filter.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FilterComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    LoginComponent,
+    RegistrationComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
