e918b604e7ad0d5800a486e5596daece4c30fb64
Denis Ivanchenko <sicknature24@yandex.com>
2023-10-17 13:06:53 +0300

    feat: add search-item and search-response models

---
+++ src/app/models/search-item.model.ts
@@ -0,0 +1,48 @@
+export interface ISearchItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: ISnippet;
+  statistics: IStatistics;
+}
+
+interface ISnippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: IThumbnails;
+  channelTitle: string;
+  tags: string[];
+  categoryId: string;
+  liveBroadcastContent: string;
+  localized: ILocalized;
+  defaultAudioLanguage: string;
+}
+
+interface IThumbnails {
+  default: IThumbnail;
+  medium: IThumbnail;
+  high: IThumbnail;
+  standard: IThumbnail;
+  maxres: IThumbnail;
+}
+
+interface IThumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface ILocalized {
+  title: string;
+  description: string;
+}
+
+interface IStatistics {
+  viewCount: string;
+  likeCount: string;
+  dislikeCount: string;
+  favoriteCount: string;
+  commentCount: string;
+}
---
+++ src/app/models/search-response.model.ts
@@ -0,0 +1,13 @@
+import { ISearchItem } from './search-item.model';
+
+export interface ISearchResponse {
+  kind: string;
+  etag: string;
+  pageInfo: IPageInfo;
+  items: ISearchItem;
+}
+
+interface IPageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
