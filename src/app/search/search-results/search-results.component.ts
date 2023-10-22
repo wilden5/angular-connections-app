19c72f3b949524994a172cedab90ac4b80b89cc3
Denis Ivanchenko <sicknature24@yandex.com>
2023-10-22 16:15:51 +0300

    feat: add YoutubeItemService to separate receiving data

--- src/app/search/search-results/search-results.component.ts
+++ src/app/search/search-results/search-results.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { youtubeResponse } from '../../../mocks/youtube.mock';
+import { Component, OnInit } from '@angular/core';
 import { ISearchItem } from '../../models/search-item.model';
+import { YoutubeItemService } from '../../services/youtube-item.service';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss'],
 })
-export class SearchResultsComponent {
-  mockArray: ISearchItem[] = youtubeResponse.items;
+export class SearchResultsComponent implements OnInit {
+  itemsArray: ISearchItem[] = [];
+
+  filteredItemsArray: ISearchItem[] = [];
+
+  constructor(private youtubeItemService: YoutubeItemService) {}
+
+  ngOnInit(): void {
+    this.youtubeItemService.getYoutubeItems().subscribe((data) => {
+      this.itemsArray = data;
+      this.filteredItemsArray = data;
+    });
+  }
 }
---
+++ src/app/services/youtube-item.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { ISearchItem } from '../models/search-item.model';
+import { youtubeResponse } from '../../mocks/youtube.mock';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class YoutubeItemService {
+  getYoutubeItems(): Observable<ISearchItem[]> {
+    return of(youtubeResponse.items);
+  }
+}
