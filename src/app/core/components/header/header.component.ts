37a7b7ae1a106317d061ae3b9a277a2e84ad4ebd
Denis Ivanchenko <sicknature24@yandex.com>
2023-10-27 21:41:02 +0300

    fix: issue with toggle filters button

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { FiltersVisibilityService } from '../../../youtube/services/filters-visibility.service';
 import { SearchService } from '../../../youtube/services/search.service';
 
@@ -12,14 +13,24 @@ export class HeaderComponent {
   constructor(
     private filtersVisibilityService: FiltersVisibilityService,
     private searchService: SearchService,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar
   ) {}
 
   onToggleFiltersButtonClick(): void {
-    this.filtersVisibilityService.toggleFiltersVisibility();
+    if (this.filtersVisibilityService.isSearchUsed) {
+      this.filtersVisibilityService.toggleFiltersVisibility();
+    } else {
+      this.snackBar.open('Please use search first to enable query filters!', '', {
+        duration: 1500,
+        horizontalPosition: 'right',
+        verticalPosition: 'top',
+      });
+    }
   }
 
   onSearchButtonClick(searchQuery: string): void {
+    this.filtersVisibilityService.toggleSearchUsed();
     this.router.navigate(['/search']);
     this.searchService.setSearchObservable(searchQuery);
   }
--- src/app/youtube/services/filters-visibility.service.ts
+++ src/app/youtube/services/filters-visibility.service.ts
@@ -6,7 +6,13 @@ import { Injectable } from '@angular/core';
 export class FiltersVisibilityService {
   isFiltersVisible = false;
 
+  isSearchUsed = false;
+
   toggleFiltersVisibility(): void {
     this.isFiltersVisible = !this.isFiltersVisible;
+  }
+
+  toggleSearchUsed(): void {
+    this.isSearchUsed = !this.isSearchUsed;
   }
 }
