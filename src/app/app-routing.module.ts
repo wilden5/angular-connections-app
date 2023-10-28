e98117834a3432d673d48a0c74db8c38a490c344
Denis Ivanchenko <sicknature24@yandex.com>
2023-10-28 12:47:49 +0300

    feat: add 404 page, main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/pages/not-found/not-found.component';
+import { MainComponent } from './core/pages/main/main.component';
 
 const routes: Routes = [
+  { path: '', component: MainComponent },
   { path: 'search', loadChildren: () => import('./youtube/youtube.module').then((m) => m.YoutubeModule) },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
 import { CustomButtonComponent } from '../shared/components/custom-button.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  imports: [CommonModule, CustomButtonComponent, NgOptimizedImage, SharedModule],
+  declarations: [HeaderComponent, NotFoundComponent, MainComponent],
+  imports: [CommonModule, CustomButtonComponent, NgOptimizedImage, SharedModule, RouterLink],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
