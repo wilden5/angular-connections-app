88767968c02d2c6a85edfb092e120aebccc3cc68
Denis Ivanchenko <sicknature24@yandex.com>
2023-10-28 21:35:08 +0300

    feat: add getSpecificItemById method to youtube service, add basic logic to detailed-information component

--- src/app/youtube/pages/detailed-information/detailed-information.component.ts
+++ src/app/youtube/pages/detailed-information/detailed-information.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ISearchItem } from '../../models/search-item.model';
+import { YoutubeItemService } from '../../services/youtube-item.service';
 
 @Component({
   selector: 'app-detailed-information',
   templateUrl: './detailed-information.component.html',
   styleUrls: ['./detailed-information.component.scss'],
 })
-export class DetailedInformationComponent {}
+export class DetailedInformationComponent implements OnInit {
+  @Input() searchItem: ISearchItem | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private youtubeItemService: YoutubeItemService
+  ) {}
+
+  ngOnInit(): void {
+    const itemId = this.route.snapshot.paramMap.get('id');
+    if (itemId) {
+      this.youtubeItemService.getSpecificItemById(itemId).subscribe((data) => {
+        this.searchItem = data;
+      });
+    }
+  }
+}
--- src/app/youtube/services/youtube-item.service.ts
+++ src/app/youtube/services/youtube-item.service.ts
@@ -10,4 +10,8 @@ export class YoutubeItemService {
   getYoutubeItemsBySearchQuery(query: string): Observable<ISearchItem[]> {
     return of(youtubeResponse.items.filter((item) => item.snippet.title.toLowerCase().includes(query.toLowerCase())));
   }
+
+  getSpecificItemById(id: string): Observable<ISearchItem | undefined> {
+    return of(youtubeResponse.items.find((item) => item.id === id));
+  }
 }
