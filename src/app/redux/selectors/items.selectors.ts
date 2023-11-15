07316133667014744daf6bb1b0ea2e4238dffdec
Denis Ivanchenko <sicknature24@yandex.com>
2023-11-15 23:44:27 +0300

    feat: update items selectors

--- src/app/redux/selectors/items.selectors.ts
+++ src/app/redux/selectors/items.selectors.ts
@@ -2,11 +2,10 @@ import { createSelector } from '@ngrx/store';
 import { AppState } from '../app.state';
 import { ISearchItem } from '../../youtube/models/search-item.model';
 
-export const selectYoutubeItems = (state: AppState): ISearchItem[] => state.youtubeItems;
+export const selectVideoItems = (state: AppState): { [id: string]: ISearchItem } => state.videoItems;
 
-export const selectCustomItems = (state: AppState): ISearchItem[] => state.customItems;
+export const selectVideoListIds = (state: AppState): string[] => state.videoListIds;
 
-export const selectAllItems = createSelector(selectYoutubeItems, selectCustomItems, (youtubeItems, customItems) => [
-  ...customItems,
-  ...youtubeItems,
-]);
+export const selectAllItems = createSelector(selectVideoItems, selectVideoListIds, (videoItems, videoListIds) =>
+  videoListIds ? videoListIds.map((id) => videoItems[id]) : []
+);
