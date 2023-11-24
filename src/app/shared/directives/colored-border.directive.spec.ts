039b8d3efb41bfb044be6647ebbb1d109ee740ed
Denis Ivanchenko <sicknature24@yandex.com>
2023-11-24 15:29:55 +0300

    refactor: delete default test files for angular entities which are not planned to be tested

--- src/app/shared/directives/colored-border.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ColoredBorderDirective } from './colored-border.directive';
-
-describe('ColoredBorderDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ColoredBorderDirective();
-    expect(directive).toBeTruthy();
-  });
-});
