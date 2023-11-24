abd67caa0408b5f3544c46f7930f5c244ca19985
Denis Ivanchenko <sicknature24@yandex.com>
2023-11-24 13:40:55 +0300

    feat: add unit tests for header,not-found and filter components

--- src/app/core/pages/not-found/not-found.component.spec.ts
+++ src/app/core/pages/not-found/not-found.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NgOptimizedImage } from '@angular/common';
+import { ActivatedRoute, convertToParamMap, RouterLink } from '@angular/router';
+import { of } from 'rxjs';
 import { NotFoundComponent } from './not-found.component';
 
 describe('NotFoundComponent', () => {
@@ -9,6 +12,8 @@ describe('NotFoundComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [NotFoundComponent],
+      imports: [NgOptimizedImage, RouterLink],
+      providers: [{ provide: ActivatedRoute, useValue: { paramMap: of(convertToParamMap({})) } }],
     });
     fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
@@ -17,5 +22,11 @@ describe('NotFoundComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a link to main page', () => {
+    const mainLink = fixture.nativeElement.querySelector('.home-link');
+    expect(mainLink.textContent).toContain('home');
+    expect(mainLink.getAttribute('href')).toEqual('/');
   });
 });
--- src/app/youtube/components/filter/filter.component.spec.ts
+++ src/app/youtube/components/filter/filter.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { By } from '@angular/platform-browser';
 import { FilterComponent } from './filter.component';
+import { CustomButtonComponent } from '../../../shared/components/custom-button.component';
 
 describe('FilterComponent', () => {
   let component: FilterComponent;
@@ -9,6 +11,7 @@ describe('FilterComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [FilterComponent],
+      imports: [CustomButtonComponent],
     });
     fixture = TestBed.createComponent(FilterComponent);
     component = fixture.componentInstance;
@@ -17,5 +20,23 @@ describe('FilterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should send sortByViewsRequested event', () => {
+    const event = jest.spyOn(component.sortByViewsRequested, 'emit');
+    const sortByViewsButton = fixture.debugElement.query(By.css('.sort-by-views-button'));
+    sortByViewsButton.triggerEventHandler('buttonClickRequested', null);
+
+    expect(event).toHaveBeenCalled();
+  });
+
+  it('should send onSortByKeywordInputChange event', () => {
+    const searchQuery = 'test';
+    const event = jest.spyOn(component.sortByKeyWordRequested, 'emit');
+    const filterInput = fixture.nativeElement.querySelector('.filter-input');
+    filterInput.value = searchQuery;
+    filterInput.dispatchEvent(new Event('keyup'));
+
+    expect(event).toHaveBeenCalledWith(searchQuery);
   });
 });
