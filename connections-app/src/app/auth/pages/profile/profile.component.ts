6b8b2a61e5395fda0869620fc634adb688d2ff0c
Denis Ivanchenko <sicknature24@yandex.com>
2023-12-03 12:59:06 +0300

    feat: extend profile component markup, add restoring store value on cancel button click

--- connections-app/src/app/auth/pages/profile/profile.component.ts
+++ connections-app/src/app/auth/pages/profile/profile.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { take } from 'rxjs';
 import { loadProfile } from '../../../redux/actions/user.actions';
-import { selectUser } from '../../../redux/selectors/user.selectors';
+import { selectUser, selectUserName } from '../../../redux/selectors/user.selectors';
 
 @Component({
   selector: 'app-profile',
@@ -10,11 +11,31 @@ import { selectUser } from '../../../redux/selectors/user.selectors';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProfileComponent implements OnInit {
+  @ViewChild('inputName') inputName!: ElementRef;
+
   protected readonly selectUser = selectUser;
+
+  protected isDisabled = true;
 
   constructor(protected store: Store) {}
 
   ngOnInit(): void {
     this.store.dispatch(loadProfile());
   }
+
+  onEditButtonClick(): void {
+    this.isDisabled = !this.isDisabled;
+  }
+
+  onCancelButtonClick(): void {
+    this.store
+      .select(selectUserName)
+      .pipe(take(1))
+      .subscribe((value) => {
+        this.inputName.nativeElement.value = value;
+      });
+    this.isDisabled = !this.isDisabled;
+  }
+
+  onSaveButtonClick(): void {}
 }
--- connections-app/src/app/redux/selectors/user.selectors.ts
+++ connections-app/src/app/redux/selectors/user.selectors.ts
@@ -4,3 +4,5 @@ import { RootState } from '../root.state';
 export const selectRootState = createFeatureSelector<RootState>('user');
 
 export const selectUser = createSelector(selectRootState, (state) => state.user);
+
+export const selectUserName = createSelector(selectRootState, (state) => state.user?.name);
